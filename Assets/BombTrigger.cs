83ec3cf42b93e27e337e43774e47bece19d115f8
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-06-29 14:45:37 +0300

    update
    -

---
+++ Assets/BombTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+
+        Debug.Log("Enter");
+
+        if (col.gameObject.tag == "Bomb")
+        {
+            Debug.Log("This is an item!");
+        }
+
+    }
+}
